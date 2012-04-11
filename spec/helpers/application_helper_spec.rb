4bbd1227126adb9ca883b21114e6331dd1caaf82
Masha <masha.ku@gmail.com>
2012-04-11 15:30:10 -0400

    Basic user Model

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,6 @@
-describe Application do
+require 'spec_helper'
+
+describe ApplicationHelper do
 	describe "full_title" do
 		it "should include the page name" do
 			full_title("foo").should =~ /foo/
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,6 @@ describe "User pages" do
 		before {visit signup_path}
 
 		it { should have_selector('h1',		text: 'Sign Up')}
-		it {should have_selector('title',	text: full_title('Sign Up'))}
+		it { should have_selector('title',	text: full_title('Sign Up'))}
 	end
 end
