3c2742fca6f430fd1d9b1dc9de513b80153fa2cc
Masha <masha.ku@gmail.com>
2012-04-17 10:47:33 -0400

    Complete feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,14 @@ class User < ActiveRecord::Base
   has_secure_password
   before_save :create_member_token
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reversed_relationships, foreign_key: "followed_id", 
+                                    class_name: "Relationship",
+                                    dependent: :destroy
+  has_many :followers, through: :reversed_relationships
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
@@ -22,8 +30,22 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6}
   validates :password_confirmation, presence: true
 
+  scope :admin, where(admin: true)
+
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,18 +56,29 @@ describe "Static Pages" do
   end
 
   describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
+
+    describe "follower/following counts" do
+      let(:other_user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
+        other_user.follow!(user)
         visit root_path
       end
 
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
-        end
-      end
+      it { should have_link("0 following", href: following_user_path(user)) }
+      it { should have_link("1 follower",  href: followers_user_path(user)) }
     end
+  end
 end
